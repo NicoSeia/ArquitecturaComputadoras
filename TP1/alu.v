b2b0e60223db143c92448a457e085f6ad2c21f5d
Nicolas Seia <nicolasseia0@gmail.com>
2025-09-06 18:32:50 -0300

    ALU implemented

--- TP1/alu.v
+++ TP1/alu.v
@@ -21,6 +21,7 @@ module alu (
     localparam DATA_2       = [NB_DATA - 1:0];
     localparam DATA_3       = [NB_DATA - 3:0];
     localparam ALU_RESULT   = [NB_DATA - 1:0];
+    localparam ALU_OP_CARRY = [NB_DATA : 0];
 
     // Data assignments
     always @(*) begin
@@ -44,15 +45,42 @@ module alu (
             o_zero <= 1'b0;
         end
         case (DATA_3)
-            6'b100000: {o_carry, o_zero, ALU_RESULT} <= DATA_1 + DATA_2;                             // ADD
-            6'b100010: {o_carry, o_zero, ALU_RESULT} <= DATA_1 - DATA_2;                             // SUB
-            6'b100100: {o_carry, o_zero, ALU_RESULT} <= DATA_1 & DATA_2;                             // AND
-            6'b100101: {o_carry, o_zero, ALU_RESULT} <= DATA_1 | DATA_2;                             // OR
-            6'b100111: {o_carry, o_zero, ALU_RESULT} <= ~(DATA_1 | DATA_2);                          // NOR
-            6'b100110: {o_carry, o_zero, ALU_RESULT} <= DATA_1 ^ DATA_2;                             // XOR
-            6'b000010: {o_carry, o_zero, ALU_RESULT} <= DATA_1 >> DATA_2[NB_DATA - 1: NB_DATA - 2];  // SRL
-            6'b000011: {o_carry, o_zero, ALU_RESULT} <= DATA_1 >>> DATA_2[NB_DATA - 1: NB_DATA - 2]; // SRA
-            default: {o_carry, o_zero, ALU_RESULT} <= {1'b0, 1'b0, 8'b0};                            // NOP
+            6'b100000: begin
+                ALU_OP_CARRY <= DATA_1 + DATA_2;                            // ADD
+                ALU_RESULT   <= ALU_OP_CARRY[NB_DATA - 1:0];
+                o_carry <= ALU_OP_CARRY[NB_DATA];
+            end
+            6'b100010: begin
+                ALU_OP_CARRY <= {1'b0, DATA_1} - {1'b0, DATA_2};            // SUB
+                ALU_RESULT   <= ALU_OP_CARRY[NB_DATA - 1:0];
+                o_carry      <= ~ALU_OP_CARRY[NB_DATA];
+            end
+            6'b100100: begin
+                ALU_RESULT <= DATA_1 & DATA_2;                              // AND
+                o_carry <= 1'b0;
+            end
+            6'b100101: begin
+                ALU_RESULT <= DATA_1 | DATA_2;                              // OR
+                o_carry <= 1'b0;
+            end
+            6'b100111: begin
+                ALU_RESULT <= ~(DATA_1 | DATA_2);                           // NOR
+                o_carry <= 1'b0;
+            end
+            6'b100110: begin
+                ALU_RESULT <= DATA_1 ^ DATA_2;                              // XOR
+                o_carry <= 1'b0;
+            end
+            6'b000010: begin
+                ALU_RESULT <= DATA_1 >> DATA_2[NB_DATA - 1: NB_DATA - 2];   // SRL
+                o_carry <= 1'b0;
+            end
+            6'b000011: begin
+                ALU_RESULT <= DATA_1 >>> DATA_2[NB_DATA - 1: NB_DATA - 2];  // SRA
+                o_carry <= 1'b0;
+            end
+            default: {o_carry, o_zero, ALU_RESULT} <= {1'b0, 1'b0, 8'b0};
         endcase
+        o_zero <= (ALU_RESULT == 8'b0) ? 1'b1 : 1'b0;
     end
 endmodule

d7933c208c12de970d181c27e72c6b6b2900bbeb
Nicolas Seia <nicolasseia0@gmail.com>
2025-09-10 18:22:01 -0300

    modificacion en el top

--- TP1/top.v
+++ TP1/top.v
@@ -4,18 +4,16 @@ module top #(
     parameter NB_DATA = 8,
     parameter NB_LEDS = 8
 ) (
-    input i_clk,
-    input i_reset,
-    input [NB_DATA - 1:0] i_data,
-    input i_enable_1,
-    input i_enable_2,
-    input i_enable_3,
-    output [NB_DATA - 1:0] o_data,
-    output o_carry,
-    output o_zero
-    output [NB_LEDS - 1:0] o_led_data,
-    output o_led_carry,
-    output o_led_zero
+    input wire i_clk,
+    input wire i_reset,
+    input wire [NB_DATA - 1:0] i_data,
+    input wire i_enable_1,
+    input wire i_enable_2,
+    input wire i_enable_3,
+
+    output wire [NB_LEDS - 1:0] o_led_data,
+    output wire o_led_carry,
+    output wire o_led_zero
 );
     alu #(
         .NB_DATA(NB_DATA)
@@ -26,12 +24,9 @@ module top #(
         .i_enable_3(i_enable_3),
         .i_clk(i_clk),
         .i_reset(i_reset),
-        .o_data(o_data),
-        .o_carry(o_carry),
-        .o_zero(o_zero)
+        .o_data(o_led_data),
+        .o_carry(o_led_carry),
+        .o_zero(o_led_zero)
     );
 
-    assign o_led_data = o_data;
-    assign o_led_carry = o_carry;
-    assign o_led_zero = o_zero;
 endmodule
